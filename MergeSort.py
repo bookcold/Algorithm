ffa8d4ae6bbfda1f2914b8f08e8053dfd14376de
bookcold <book-cold@hotmail.com>
2012-01-26 17:55:35 +0800

    Modify the MergeSort

--- MergeSort.py
+++ MergeSort.py
@@ -6,6 +6,7 @@ class MergeSort:
     def __print__(self):
         print self.array
 
+
     def MergeSorting(self,first,last):
         if first < last:
             middle = (first + last)//2
@@ -14,19 +15,26 @@ class MergeSort:
             self.Merge(first, last, middle)
 
     def Merge(self, first, last, middle):
-        leftArray = [self.array[i] for i in range(first,middle+1)]
-        rightArray = [self.array[i] for i in range(middle+1,last+1)]
-        i = 0
-        j = 0
-        for k in range(first,last):
-            if(leftArray[i] <= rightArray[j]):
-                self.array[k] = leftArray[i]
-                i +=1
+        tmpArray = []
+        i = first
+        j = middle+1
+        while i<=middle and j<=last:
+            if(self.array[i] <= self.array[j]):
+                tmpArray.append(self.array[i])
+                i += 1
             else:
-                self.array[k] = rightArray[j]
-                j +=1
+                tmpArray.append(self.array[j])
+                j += 1
+        while i<=middle:
+            tmpArray.append(self.array[i])
+            i += 1
+        while j<=last:
+            tmpArray.append(self.array[j])
+            j += 1
+        for k in range(0, last - first + 1):
+            self.array[first + k] = tmpArray[k]
 
-array = [3,5,1,7,2,6,4,8]
+array = [3,5,1,9,7,2,6,4,8,10]
 s = MergeSort(array)
 s.MergeSorting(0,len(array)-1)
 s.__print__()
